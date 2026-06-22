c31ad7fb0a4ef1f579cf107c47f86cbce73c840c
Vinit kumar <vineetpuniya392@gmail.com>
2026-06-22 09:24:03 +0530

    [Vinit Kumar] ADD : questions of array in assessement

---
+++ gcr-codebase/ProgrammingElements/Assessment/Array/Level1/StudentVoting.java
@@ -0,0 +1,30 @@
+package ProgrammingElements.Assessment.Array.Level1;
+
+import java.util.Scanner;
+
+public class StudentVoting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] ages = new int[10];
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Invalid age");
+            }
+            else if (ages[i] >= 18) {
+                System.out.println("The student with the age " + ages[i] + " can vote.");
+            }
+            else {
+                System.out.println("The student with the age " + ages[i] + " cannot vote.");
+            }
+        }
+
+        sc.close();
+    }
+}
